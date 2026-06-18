c26d299986f7306b2cfc760f82a989780b1192e4
Pranab Murmu <pranabmrmu@gmail.com>
2026-06-18 09:53:40 +0530

    Angle Between Hands of a clock

---
+++ 1344.cpp
@@ -0,0 +1,11 @@
+//Angle Between Hands of a Clock
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double minuteAngle = minutes * 6.0;
+        double hourAngle = (hour % 12) * 30.0 + minutes * 0.5;
+
+        double diff = fabs(hourAngle - minuteAngle);
+        return min(diff, 360.0 - diff);
+    }
+};
