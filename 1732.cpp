ece7586ff5fc64d782dac26069e93cbf7bfb9944
Pranab Murmu <pranabmrmu@gmail.com>
2026-06-19 20:24:24 +0530

    Implement largestAltitude function in Solution class

---
+++ 1732.cpp
@@ -0,0 +1,14 @@
+// Find The Highest Altitude
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int cur = 0, ans = 0;
+
+        for (int x : gain) {
+            cur += x;
+            ans = max(ans, cur);
+        }
+
+        return ans;
+    }
+};
