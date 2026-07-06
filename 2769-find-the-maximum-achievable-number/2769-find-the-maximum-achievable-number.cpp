2e1d91a9598d4104ffb945e44afb2d9b5e8ceb07
Pranab Murmu <pranabmrmu@gmail.com>
2026-07-06 22:25:48 +0530

    Time: 0 ms (100%), Space: 9.6 MB (44.57%) - LeetHub

---
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        int n = num;
+        int tt = t;
+
+        int x = n + (2*tt);
+
+        return x;
+        
+    }
+};
