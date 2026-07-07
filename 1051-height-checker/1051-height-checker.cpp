197bb50e03462a7d92af80918b8791abe61fceb1
Pranab Murmu <pranabmrmu@gmail.com>
2026-07-07 21:48:13 +0530

    Time: 0 ms (100%), Space: 11.5 MB (5.61%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int n = heights.size();
+        int count = 0;
+        vector<int> vec = heights;
+        sort(heights.begin(), heights.end());
+
+        for (int i = 0; i < n; i++) {
+            if (heights[i] != vec[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
