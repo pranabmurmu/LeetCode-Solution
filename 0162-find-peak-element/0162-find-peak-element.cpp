657a96c759f2f36ab7cb6157c916a32b41f7a673
Pranab Murmu <pranabmrmu@gmail.com>
2026-07-11 00:07:29 +0530

    Time: 0 ms (100%), Space: 12.7 MB (5.85%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,34 +1,51 @@
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+//         int n =nums.size();
+//         if(nums.size()==1){ //corner case where arr size is 1
+//             return 0;
+//         }
+
+//         if(nums[0]>nums[1]){ //corner case where index zero is included
+//             return 0;
+//         }
+//         if(nums[n-1]>nums[n-2]){ //corner case where index n-1 is included
+//             return n-1; 
+//         }
+         
+
+//         int st = 1;
+//         int end = nums.size() - 2;
+
+//         while (st <= end) {
+//             int mid = st + (end - st) / 2;
+//             if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+//                 return mid;
+//             }
+//             if (nums[mid - 1] < nums[mid]) {
+//                 st = mid + 1;
+//             } else {
+//                 end = mid - 1;
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n =nums.size();
-        if(nums.size()==1){
-            return 0;
+        int left = 0, right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1])
+                left = mid + 1;
+            else
+                right = mid;
         }
 
-        if(nums[0]>nums[1]){
-            return 0;
-        }
-        if(nums[n-1]>nums[n-2]){
-            return n-1;
-        }
-         
-
-        int st = 1;
-        int end = nums.size() - 2;
-
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
-                return mid;
-            }
-            if (nums[mid - 1] < nums[mid]) {
-                st = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-
-        return -1;
+        return left;
     }
 };
