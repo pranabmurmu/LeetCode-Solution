79c39b67399f87a203502d4deff659cca94f8e90
Pranab Murmu <pranabmrmu@gmail.com>
2026-07-23 16:42:11 +0530

    Time: 0 ms (100%), Space: 198.7 MB (98.13%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize("O3")
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 2) return n;
+        int p = 1;
+        while (p <= n) {
+            p <<= 1;
+        }
+        return p;
+    }
+};
