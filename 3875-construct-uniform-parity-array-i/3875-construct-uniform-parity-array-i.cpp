407aba8c87f579929894eb2b5901e38939cb32c8
Pranab Murmu <pranabmrmu@gmail.com>
2026-09-02 09:43:00 +0530

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // vector<int> ans;
+        // int n = nums1.size();
+
+        // for(int i=0; i<n;i++){
+
+
+        // }
+
+        return true;
+        
+    }
+};
